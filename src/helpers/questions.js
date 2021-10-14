f8c434097101d59663852df2ef9094b1529a2449
Henrique Dos Santos Martins <henrique0126@gmail.com>
2021-10-14 18:05:36 +0200

    'up'

--- src/helpers/questions.js
+++ src/helpers/questions.js
@@ -13,32 +13,36 @@ const itsCleanQuestions = [
   /**
    * C'est du propre
    */
-  // ! TODO
   {
     category: "C'est du propre",
-    question: "Quel est le temps de séchage d'un antiseptique alcoolique majeur lors de la préparation cutanée de l'opéré ?",
+    question: "Le temps de séchage d'un antiseptique alcoolique majeur lors de la préparation cutanée de l'opéré est de :",
     image: null,
     response: '1 minute',
-    otherResponse: []
+    otherResponse: [
+      '30 secondes',
+      '1 minute 30',
+      '2 minutes'
+    ]
   },
-  // ! TODO
   {
     category: "C'est du propre",
-    question: "Lorsqu'elle est prescrite, la dépilation doit être effectuée par:",
+    question: "Lorsqu'elle est prescrite, la dépilation doit être effectuée par :",
     image: null,
     response: 'Tonte',
     otherResponse: [
       'Rasage',
-      'Crème dépilatoire'
+      'Crème dépilatoire',
+      'Épilation'
     ]
   },
-  // ! TODO
   {
     category: "C'est du propre",
-    question: "Dans le cadre de la désinfection chirurgicale des mains, après le lavage chirurgical au savon doux, puis-je installer le patient avant l'application de la solution hydro-alcoolique ?",
+    question: "Lors de la désinfection chirurgicale des mains : je peux installer le patient, après le lavage chirurgical au savon doux et avant l'application de la solution hydro-alcoolique ?",
     image: null,
-    response: 'Oui',
-    otherResponse: []
+    response: 'Vrai',
+    otherResponse: [
+      'Faux'
+    ]
   }
 ]
 
@@ -46,13 +50,16 @@ const howItsWorksQuestions = [
   /**
    * Comment ça marche
    */
-  // ! TODO
   {
     category: 'Comment ça marche',
     question: 'Quelle est la durée maximale de gonflage du garrot chirurgical placé sur un membre supérieur ?',
     image: null,
     response: '90 min',
-    otherResponse: []
+    otherResponse: [
+      '60 min',
+      '100 min',
+      '120 min'
+    ]
   },
   {
     category: 'Comment ça marche',
@@ -61,8 +68,6 @@ const howItsWorksQuestions = [
     response: 'Dioxyde de carbone',
     otherResponse: [
       'Dioxygène',
-      'Méthane',
-      'Butane',
       "Protoxyde d'azote",
       'Air'
     ]
@@ -70,11 +75,22 @@ const howItsWorksQuestions = [
   {
     category: 'Comment ça marche',
     question: 'Afin de diminuer la dose de rayons reçus, le patient doit être placé :',
-    image: '/img/questions/Image1.png',
+    image: 'https://docteur-bron-urologue.fr/wp-content/uploads/2016/03/Lithotritie-extra-corporelle.jpg',
     response: "Au plus près du détecteur de l'amplificateur de brillance",
     otherResponse: [
       "Au plus près du tube émetteur de l'amplificateur de brillance",
       'Peu importe, la dose est la même quelle que soit sa position'
+    ]
+  },
+  {
+    category: 'Comment ça marche',
+    question: 'Que puis-je bien être ?',
+    image: '/img/questions/Image1.png',
+    response: 'Un ostéotome',
+    otherResponse: [
+      'Une rugine',
+      'Un chausse pied',
+      'Un ciseau Gouge'
     ]
   }
 ]
@@ -85,22 +101,22 @@ const onceUponATime = [
    */
   {
     category: 'Il était une fois',
-    question: "L' application du garrot pneumatique a usage chirurgical releve de :",
+    question: "L'application du garrot pneumatique à usage chirurgical est réalisée :",
     image: null,
     response: "sur prescription médicale à condition qu'un médecin puisse intervenir à tout moment",
     otherResponse: [
-      'rôle propre',
+      'sur rôle propre',
       'sur prescription médicale',
-      'rôle medical'
+      'sur rôle médical'
     ]
   },
   {
     category: 'Il était une fois',
-    question: "VRAI ou FAUX Exercer la profession infirmier sans être inscrit au tableau de l'ordre infirmier est un exercice illegal de la profession",
+    question: "Exercer la profession infirmier sans être inscrit au tableau de l'ordre infirmier est un exercice illégal de la profession",
     image: null,
-    response: 'VRAI',
+    response: 'Vrai',
     otherResponse: [
-      'FAUX'
+      'Faux'
     ]
   }
 ]
@@ -109,14 +125,14 @@ const dontPoutQuestions = [
   /**
    * Fais pas la moue
    */
-  // ! TODO
   {
     category: 'Fais pas la moue',
-    question: '(Vrai/Faux) La vésicule',
+    question: 'La vésicule biliaire :',
     image: null,
-    response: 'VRAI',
+    response: 'est vascularisée par l’artère cystique',
     otherResponse: [
-      'FAUX'
+      'est située dans l’épigastre',
+      'produit la bile'
     ]
   },
   {
@@ -130,7 +146,6 @@ const dontPoutQuestions = [
       "Pose d'une prothèse hernie de la ligne blanche"
     ]
   },
-  // ! TODO
   {
     category: 'Fais pas la moue',
     question: "En cas de sténose de l'urètre on procède en endoscopie à une :",
@@ -147,27 +162,38 @@ const fallOnABoneQuestions = [
   /**
    * Tomber sur un os
    */
-  // ! TODO
   {
     category: 'Tomber sur un os',
-    question: 'Déterminez le type de fracture dans les cas suivants :',
+    question: "L'abduction est :",
     image: null,
-    response: '',
+    response: 'L’éloignement d’un segment du membre de l’axe du corps',
     otherResponse: [
-      'Fracture diaphysaire',
-      'Fracture bifocale',
-      'Fracture comminutive',
-      'Fracture spiroide',
-      'Fracture "en bois vert"'
+      'Le rapprochement de 2 segments de membres',
+      'Le rapprochement d’un segment de membres de l’axe du corps',
+      'La déformation d’un membre'
     ]
   },
-  // ! TODO
   {
     category: 'Tomber sur un os',
-    question: "Comment se nomme l'espace entre la pie mère et l'arachnoïde :",
+    question: 'Déterminez le type de fracture dans le cas suivant :',
+    image: 'https://medicalforum.ch/fr/?tx_swajournal_file%5Baction%5D=index&tx_swajournal_file%5Bcontroller%5D=File&tx_swajournal_file%5Bidentifier%5D=fms.2020.08333&tx_swajournal_file%5Bjournal%5D=ezm_smf&tx_swajournal_file%5Blanguage%5D=fr&tx_swajournal_file%5Bmode%5D=view&tx_swajournal_file%5Bresource%5D=http%3A%2F%2Fprodclient.emh02.snowflakehosting.ch%2Fapi%2Ffile%2Fproduction%2F30%2F30f9182fd6f9c96b4df0a313ee6429374bfc880d%2F08333_de_fr_f1a_1170x450.jpg&tx_swajournal_file%5Btype%5D=article&type=7479858270737669&cHash=9097a0ec2b6b2d9b82ec100b17dd1e3a',
+    response: 'Fracture diaphysaire',
+    otherResponse: [
+      'Fracture bifocale',
+      'Fracture comminutive',
+      'Fracture spiroide'
+    ]
+  },
+  {
+    category: 'Tomber sur un os',
+    question: "L'odre des méninges de la plus externe à la plus interne est :",
     image: null,
-    response: "L'espace sous arachnoïdien",
-    otherResponse: []
+    response: 'Duremère, Arachnoïde, Pie mère',
+    otherResponse: [
+      'Pie mère, Duremère, Arachnoïde',
+      'Arachnoïde, Duremère, Pie mère',
+      'Duremère, Pie mère, Arachnoïde'
+    ]
   }
 ]
 
@@ -186,38 +212,45 @@ const infoOrBotox = [
       '20 ans'
     ]
   },
-  // ! TODO
   {
     category: 'Info ou botox',
     question: "Quel est le temps de recoloration normal d'un lambeau ?",
     image: null,
     response: '3 secondes',
-    otherResponse: []
+    otherResponse: [
+      '1 seconde',
+      '5 secondes',
+      '30 secondes'
+    ]
   },
   {
     category: 'Info ou botox',
     question: "L'installation chirurgicale du patient fait partie des actes exclusifs de l'IBODE ?",
     image: null,
-    response: 'VRAI',
+    response: 'Vrai',
     otherResponse: [
-      'FAUX'
+      'Faux'
     ]
   },
-  // ! TODO
   {
     category: 'Info ou botox',
-    question: "Lors d'une ablation de la thyroïde, quel est le risque chirurgical majeur ?",
+    question: "Lors d'une ablation de la thyroïde, le risque chirurgical majeur est la paralysie des cordes vocales par section du nerf récurrent",
     image: null,
-    response: "La paralysie des cordes vocales par section du nerf récurrent. S'il y a paralysie des cordes vocales fermées => trachéotomie en urgence !",
-    otherResponse: []
+    response: 'Vrai',
+    otherResponse: [
+      'Faux'
+    ]
   },
-  // ! TODO
   {
     category: 'Info ou botox',
     question: 'Si je suis sténosée, je peux créer un AVC.',
     image: null,
     response: "L'artère carotide interne",
-    otherResponse: []
+    otherResponse: [
+      "L'aorte",
+      "L'atère ulnaire",
+      "L'artère pylorique"
+    ]
   }
 ]
 
